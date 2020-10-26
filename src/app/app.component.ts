033e35a7fb30fb0a7c11b9f0916549cab6531097
Vipul Daripkar <vipuldaripkar@gmail.com>
2020-10-26 12:58:45 +0100

    added animate keyframes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {
   animate,
+  keyframes,
   state,
   style,
   transition,
@@ -15,22 +16,19 @@ import { Component } from '@angular/core';
     trigger('divState', [
       state(
         'normal',
-        style({ 'background-color': 'red', transform: 'translateX(0)' })
+        style({ backgroundColor: 'red', transform: 'translateX(0)' })
       ),
       state(
         'highlighted',
         style({ backgroundColor: 'blue', transform: 'translateX(100px)' })
       ),
-      transition('normal <=> highlighted', animate(300)),
-      // transition('highlighted => normal', animate(800)),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
     ]),
     trigger('wildState', [
       state(
         'normal',
-        style({
-          'background-color': 'red',
-          transform: 'translateX(0) scale(1)',
-        })
+        style({ backgroundColor: 'red', transform: 'translateX(0) scale(1)' })
       ),
       state(
         'highlighted',
@@ -48,7 +46,42 @@ import { Component } from '@angular/core';
       ),
       transition('normal => highlighted', animate(300)),
       transition('highlighted => normal', animate(800)),
-      transition('shrunken <=> *', animate(500)),
+      transition('shrunken <=> *', [
+        style({ backgroundColor: 'orange' }),
+        animate(1000, style({ borderRadius: '50px' })),
+        animate(500),
+      ]),
+    ]),
+    trigger('list1', [
+      state('in', style({ opacity: 1, transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ opacity: 0, transform: 'translateX(-300px)' }),
+        animate(300),
+      ]),
+      transition('* => void', [
+        animate(300, style({ transform: 'translateX(100px)', opacity: 0 })),
+      ]),
+    ]),
+    trigger('list2', [
+      state('in', style({ opacity: 1, transform: 'translateX(0)' })),
+      transition('void => *', [
+        animate(
+          1000,
+          keyframes([
+            style({ transform: 'translateX(-100px)', opacity: 0, offset: 0 }),
+            style({
+              transform: 'translateX(-50px)',
+              opacity: 0.5,
+              offset: 0.3,
+            }),
+            style({ transform: 'translateX(-20px)', opacity: 1, offset: 0.8 }),
+            style({ transform: 'translateX(0px)', opacity: 1, offset: 1 }),
+          ])
+        ),
+      ]),
+      transition('* => void', [
+        animate(300, style({ transform: 'translateX(100px)', opacity: 0 })),
+      ]),
     ]),
   ],
 })
